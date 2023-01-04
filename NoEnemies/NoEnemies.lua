492ec65dc9006525bbee58f7e825f1bd80a03574
Pseudonym777 <49264785+Pseudonym777@users.noreply.github.com>
2023-01-03 23:08:55 -0800

    Add files via upload

--- NoEnemies/NoEnemies.lua
+++ NoEnemies/NoEnemies.lua
@@ -1,18 +1,36 @@
 ModUtil.RegisterMod("NoEnemies")
 
-ModUtil.WrapBaseFunction( "CreateRoom", function(baseFunc, roomData, args)
-	--shop instead of empty so that cerberus functions (also fixed weird pot bug? no idea what that was)
-	ForceNextEncounter = "Shop" 
-	--remove barge softlock
-	Destroy({ Ids = GetIds({ Name = "WaterEndingObjectsDestroy" }) })
-	return baseFunc(roomData, args)
-end, NoEnemies)
+ModUtil.LoadOnce( function()
+  ModUtil.MapSetTable(EncounterData.Empty,{UnthreadedEvents = EncounterSets.EncounterEventsNonCombat})
+end)
+
+function FinishRun()
+	AddTimerBlock( CurrentRun, "done")
+	ShowRunClearScreen()
+end
 
 ModUtil.WrapBaseFunction( "ChooseRoomReward", function(baseFunc, roomData, args)
 	return nil
 end, NoEnemies)
 
 ModUtil.WrapBaseFunction( "BossIntro", function(baseFunc, eventSource, args)
+
 	--fix camera after boss fights, end of intro function usually does this
-	LockCamera({ Id = CurrentRun.Hero.ObjectId, Duration = args.DurationOut or 1.25, EaseIn = 0.04, EaseOut = 0.275 }) 
+	LockCamera({ Id = CurrentRun.Hero.ObjectId, Duration = args.DurationOut or 1.25, EaseIn = 0.04, EaseOut = 0.275 })
+
+	if CurrentRun.CurrentRoom.Name == "D_Boss01" then
+		FinishRun()
+	end
+end, NoEnemies)
+
+--fix crash in alecto/tis
+ModUtil.WrapBaseFunction( "MultiFuryIntro", function(baseFunc, eventSource, args) 
+end, NoEnemies)
+
+ModUtil.WrapBaseFunction( "DoPatches", function(baseFunc) 
+	baseFunc()
+	--nil check for fresh file
+	if CurrentRun ~= nil then
+		CurrentRun.CurrentRoom.Encounter = EncounterData.Empty
+	end
 end, NoEnemies)
